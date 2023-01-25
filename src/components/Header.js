5564c95d0cf07f03935a2ea29d85435fbaa6737b
barnaba <barnmutahi@gmail.com>
2023-01-25 09:55:50 +0300

    Latest updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { Link, useHistory } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 function Header() {
-    return (
-        <div className='container'>
-            <nav className="nav">
-  <a className="nav-link active" href="#">Active</a>
-  <a className="nav-link" href="#">Link</a>
-  <a className="nav-link" href="#">Link</a>
-  <a className="nav-link disabled">Disabled</a>
-</nav>
-        </div>
-    )
+  return (
+    <div className="container">
+      <nav className="nav">
+        <a className="nav-link active" href="#">
+          Active
+        </a>
+        <a className="nav-link" href="#">
+          Link
+        </a>
+        <a className="nav-link" href="#">
+          Link
+        </a>
+        <a className="nav-link disabled">Disabled</a>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
